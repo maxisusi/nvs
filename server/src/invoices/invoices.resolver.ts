f32cd9cf760dc37bf6df6a9d0b281695d63ba65b
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-16 15:26:19 +0200

    Generation resources for Invoices

---
+++ server/src/invoices/invoices.resolver.ts
@@ -0,0 +1,34 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { InvoicesService } from './invoices.service';
+import { CreateInvoiceInput } from './dto/create-invoice.input';
+import { UpdateInvoiceInput } from './dto/update-invoice.input';
+
+@Resolver('Invoice')
+export class InvoicesResolver {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @Mutation('createInvoice')
+  create(@Args('createInvoiceInput') createInvoiceInput: CreateInvoiceInput) {
+    return this.invoicesService.create(createInvoiceInput);
+  }
+
+  @Query('invoices')
+  findAll() {
+    return this.invoicesService.findAll();
+  }
+
+  @Query('invoice')
+  findOne(@Args('id') id: number) {
+    return this.invoicesService.findOne(id);
+  }
+
+  @Mutation('updateInvoice')
+  update(@Args('updateInvoiceInput') updateInvoiceInput: UpdateInvoiceInput) {
+    return this.invoicesService.update(updateInvoiceInput.id, updateInvoiceInput);
+  }
+
+  @Mutation('removeInvoice')
+  remove(@Args('id') id: number) {
+    return this.invoicesService.remove(id);
+  }
+}
