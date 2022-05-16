dd92425a3d5022bbffec399b2e04acd9b3f29254
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-16 10:46:16 +0200

    Company ressources works

--- src/company/dto/update-company.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateCompanyInput } from './create-company.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateCompanyInput extends PartialType(CreateCompanyInput) {
-  id: number;
-}
