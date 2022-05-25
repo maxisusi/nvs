7ae2b1fdf8a1b84834511c40276e1a9d0bc71594
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-25 15:18:27 +0200

    Adding Material ICONS

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
 
 type Props = {};
@@ -5,11 +6,11 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className='bg-skin-fill h-16 w-screen flex items-center justify-around'>
-      <div className='flex justify-between w-full max-w-screen-2xl'>
+      <div className='flex justify-between w-full max-w-screen-2xl items-center'>
         <h1 className='text-skin-white'>KREATER</h1>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 items-center'>
           <h1 className='text-skin-white'> hello mate</h1>
-          <h1 className='text-skin-white'> hello mate</h1>
+          <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
         </div>
       </div>
     </div>
