bbfb7c1555a441af0c6b59c9a3466322f608ae76
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-26 09:52:07 +0200

    Menu change state based on the page

--- client/components/menu/MenuBar.tsx
+++ client/components/menu/MenuBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MenuList from './menu-list/MenuList';
 import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
@@ -9,12 +9,14 @@ const menu = [
     name: 'clients',
     icon: <AccountCircleOutlinedIcon />,
     link: `/client`,
+    active: false,
   },
   {
     id: 1,
     name: 'invoices',
     icon: <AssessmentOutlinedIcon />,
     link: `/invoice`,
+    active: false,
   },
 ];
 
@@ -22,13 +24,21 @@ const MenuBar = () => {
   const router = useRouter();
   const handleChangePage = (link: string): void => {
     router.push(link);
+
+    menu.map((e) => {
+      if (e.link === link) {
+        e.active = true;
+      } else {
+        e.active = false;
+      }
+    });
   };
 
   return (
     <div className='w-60 pt-6 h-screen drop-shadow-xl bg-white'>
       {menu.map((item) => (
         <div key={item.id} onClick={() => handleChangePage(item.link)}>
-          <MenuList icon={item.icon} name={item.name} />
+          <MenuList icon={item.icon} name={item.name} active={item.active} />
         </div>
       ))}
     </div>
