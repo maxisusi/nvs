39db83e1201a9e0ad29feec7e132e03d5b0344e1
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-27 11:59:06 +0200

    Main style for customer list done

--- client/components/customerList/customerList.tsx
+++ client/components/customerList/customerList.tsx
@@ -4,11 +4,13 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 type Props = {};
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
   {
     field: 'Name',
     headerName: 'Name',
     flex: 1,
+    cellClassName: 'name-style',
+    headerClassName: 'MuiDataGrid-columnHeaders',
+    disableColumnMenu: true,
     valueGetter: (params: GridValueGetterParams) =>
       `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
@@ -16,17 +18,28 @@ const columns: GridColDef[] = [
     field: 'phone',
     headerName: 'Phone',
     flex: 1,
+    disableColumnMenu: true,
+    headerAlign: 'left',
+    cellClassName: 'field-style',
+    headerClassName: 'MuiDataGrid-columnHeaders',
   },
   {
     field: 'amountDue',
     headerName: 'Amount Due',
-    type: 'number',
+    headerAlign: 'left',
+    disableColumnMenu: true,
     flex: 1,
+    cellClassName: 'field-style',
+    headerClassName: 'MuiDataGrid-columnHeaders',
   },
   {
     field: 'createdOn',
     headerName: 'Created On',
     flex: 1,
+    disableColumnMenu: true,
+    headerAlign: 'left',
+    cellClassName: 'field-style',
+    headerClassName: 'MuiDataGrid-columnHeaders',
   },
 ];
 
@@ -35,7 +48,7 @@ const rows = [
     id: 1,
     lastName: 'Snow',
     firstName: 'Jon',
-    amountDue: 254,
+    amountDue: '254',
     phone: '079227817',
     createdOn: '01/05/1999',
   },
@@ -44,14 +57,54 @@ const rows = [
     lastName: 'Lannister',
     firstName: 'Cersei',
     phone: '079227817',
-    amountDue: 3823782,
+    amountDue: '3823782',
     createdOn: '01/05/1999',
   },
   {
     id: 3,
     lastName: 'Edward',
     firstName: 'London',
-    amountDue: 27381,
+    amountDue: '27381',
+    phone: '089382782',
+    createdOn: '01/05/1999',
+  },
+  {
+    id: 4,
+    lastName: 'Edward',
+    firstName: 'London',
+    amountDue: '27381',
+    phone: '089382782',
+    createdOn: '01/05/1999',
+  },
+  {
+    id: 5,
+    lastName: 'Snow',
+    firstName: 'Jon',
+    amountDue: '254',
+    phone: '079227817',
+    createdOn: '01/05/1999',
+  },
+  {
+    id: 6,
+    lastName: 'Lannister',
+    firstName: 'Cersei',
+    phone: '079227817',
+    amountDue: '3823782',
+    createdOn: '01/05/1999',
+  },
+  {
+    id: 7,
+    lastName: 'Edward',
+    firstName: 'London',
+    amountDue: '27381',
+    phone: '089382782',
+    createdOn: '01/05/1999',
+  },
+  {
+    id: 8,
+    lastName: 'Edward',
+    firstName: 'London',
+    amountDue: '27381',
     phone: '089382782',
     createdOn: '01/05/1999',
   },
@@ -60,14 +113,51 @@ const rows = [
 const CustomerList = (props: Props) => {
   return (
     <>
-      <div style={{ height: 400, width: '100%' }}>
+      <div style={{ width: '100%' }}>
         <DataGrid
           rows={rows}
+          autoHeight
           columns={columns}
+          density='comfortable'
           pageSize={5}
           rowsPerPageOptions={[5]}
           checkboxSelection
           disableSelectionOnClick
+          sx={{
+            backgroundColor: 'white',
+            fontFamily: 'unset',
+
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: '#F5F6FA',
+              color: '#64748b',
+              minHeight: '3em !important',
+              maxHeight: '3em !important',
+              textTransform: 'uppercase',
+              fontSize: '.75rem',
+              letterSpacing: '1px',
+            },
+
+            '& .MuiDataGrid-virtualScroller': {
+              marginTop: '35px !important',
+            },
+            '& .name-style': {
+              fontWeight: '500',
+
+              fontSize: '0.95rem',
+              cursor: 'pointer',
+            },
+            '& .field-style': {
+              color: '#58667a',
+              fontWeight: 'light',
+            },
+
+            '& .MuiDataGrid-row:nth-child(even)': {
+              backgroundColor: '#f8fafc',
+            },
+            '& .MuiDataGrid-cell': {
+              border: 'none',
+            },
+          }}
         />
       </div>
     </>
