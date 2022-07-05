af498fabb32ef3d59a8a4ce24fac0b641ff1d7a2
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-07-05 09:43:59 +0200

    Invoice can be removed from invoice view page

--- client/pages/invoice/view/[id].tsx
+++ client/pages/invoice/view/[id].tsx
@@ -1,4 +1,6 @@
 import {
+  DEL_INVOICE,
+  GET_INVOICES_FOR_GRID,
   GET_INVOICE_VIEW,
   UPDATE_INVOICE_STATUS,
 } from '@nvs-shared/graphql/invoice';
@@ -10,6 +12,19 @@ import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import { useRouter } from 'next/router';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { useMutation } from '@apollo/client';
+import {
+  Box,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Modal,
+} from '@mui/material';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
+import { Invoice } from '@nvs-shared/types/invoice';
 
 const invoiceViewPage = (props: $TSFixIt) => {
   const {
@@ -28,8 +43,27 @@ const invoiceViewPage = (props: $TSFixIt) => {
   const router = useRouter();
   console.log(props);
 
+  // * Menu State manager
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleCloseMenu = () => setAnchorEl(null);
+  const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // * Modal State manager
+  const [openModal, setOpenModal] = React.useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
   const [invoiceStatus, setInvoiceStatus] = useState(status);
   const [updateInvoiceStatus] = useMutation(UPDATE_INVOICE_STATUS);
+  const [removeInvoice] = useMutation(DEL_INVOICE);
+
+  const handleOpenDeleteInvoiceModal = () => {
+    handleCloseMenu();
+    handleOpenModal();
+  };
 
   const handleInvoiceStatus = async (status: string) => {
     setInvoiceStatus(status);
@@ -46,6 +80,36 @@ const invoiceViewPage = (props: $TSFixIt) => {
     } catch (e) {
       alert('There was an error, please check the console for further details');
       console.error(e);
+    }
+  };
+
+  const handleDeleteInvoice = async (invoiceId: string) => {
+    try {
+      await removeInvoice({
+        variables: { removeInvoiceId: invoiceId },
+        update: (store) => {
+          const previousData: any = store.readQuery({
+            query: GET_INVOICES_FOR_GRID,
+          });
+
+          store.writeQuery({
+            query: GET_INVOICES_FOR_GRID,
+            data: {
+              invoices: previousData.invoices.filter(
+                (invoice: Invoice) => invoice.id !== invoiceId
+              ),
+            },
+            overwrite: true,
+          });
+        },
+      }).then(() => {
+        handleCloseModal();
+        router.push(`/customer/view/${customer.id}`);
+      });
+    } catch (e) {
+      alert('There was an error, please check the console for further details');
+      console.error(e);
+      handleCloseModal();
     }
   };
   return (
@@ -72,7 +136,9 @@ const invoiceViewPage = (props: $TSFixIt) => {
             <PictureAsPdfIcon />
             Download
           </button>
-          <button className='bg-skin-fill font-semibold text-skin-white px-3 py-2 rounded text-sm hover:bg-skin-btnHover drop-shadow-md flex gap-2 items-center'>
+          <button
+            onClick={handleOpenMenu}
+            className='bg-skin-fill font-semibold text-skin-white px-3 py-2 rounded text-sm hover:bg-skin-btnHover drop-shadow-md flex gap-2 items-center'>
             <MoreHorizIcon />
           </button>
         </div>
@@ -170,6 +236,81 @@ const invoiceViewPage = (props: $TSFixIt) => {
           </tbody>
         </table>
       </div>
+
+      <Menu
+        id='invoice-menu'
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={open}
+        onClose={handleCloseMenu}
+        sx={menuStyle}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}>
+        <MenuItem>
+          <ListItemIcon>
+            <EditOutlinedIcon className='text-skin-gray' fontSize='small' />
+          </ListItemIcon>
+          <ListItemText className='text-skin-base'>Edit</ListItemText>
+        </MenuItem>
+        {/* <MenuItem>
+          <ListItemIcon>
+            <VisibilityOutlinedIcon
+              className='text-skin-gray'
+              fontSize='small'
+            />
+          </ListItemIcon>
+          <ListItemText>View</ListItemText>
+        </MenuItem> */}
+        <MenuItem onClick={() => handleOpenDeleteInvoiceModal()}>
+          <ListItemIcon>
+            <DeleteOutlineOutlinedIcon
+              className='text-skin-gray'
+              fontSize='small'
+            />
+          </ListItemIcon>
+          <ListItemText>Delete</ListItemText>
+        </MenuItem>
+      </Menu>
+
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={modalStyle}>
+          <div className='flex justify-center '>
+            <div className='bg-red-100 p-3 rounded-full'>
+              <WarningAmberOutlinedIcon className='text-red-500  text-3xl' />
+            </div>
+          </div>
+          <div className='flex justify-center mt-6 flex-col text-center gap-2'>
+            <h3 className='text-xl text-skin-base'>Are you sure?</h3>
+            <p className='text-skin-gray text-sm'>
+              You will not be able to recover this invoice.
+            </p>
+            <div className='flex gap-3 mt-5'>
+              <button
+                onClick={() => handleDeleteInvoice(id)}
+                className='bg-red-500 hover:bg-red-600 text-skin-white w-full rounded py-2'>
+                Ok
+              </button>
+              <button
+                onClick={handleCloseModal}
+                className='border hover:bg-slate-100 text-skin-base w-full rounded py-2'>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Box>
+      </Modal>
     </>
   );
 };
@@ -218,4 +359,27 @@ const displayInvoiceStatus = (status: string) => {
       {status}
     </div>
   );
+};
+
+const menuStyle = {
+  '& .MuiPaper-root': {
+    marginTop: '10px',
+    boxShadow: 'none',
+    filter:
+      'drop-shadow(0 1px 2px rgb(0 0 0 / 0.1)) drop-shadow(0 1px 1px rgb(0 0 0 / 0.06));',
+    width: '150px',
+  },
+};
+
+const modalStyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  borderRadius: '0.5rem',
+  bgcolor: 'background.paper',
+  filter:
+    'drop-shadow(0 1px 2px rgb(0 0 0 / 0.1)) drop-shadow(0 1px 1px rgb(0 0 0 / 0.06))',
+  p: 4,
 };
